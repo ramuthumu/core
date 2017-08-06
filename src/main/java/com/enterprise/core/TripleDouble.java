61d6873cba3eecefcc625bac90a7f4e386da546b
Ramu Thumu <ramuthumu@Ramus-MacBook-Pro.local>
2017-08-06 17:37:25 -0500

    updated comments

--- src/main/java/com/enterprise/core/TripleDouble.java
+++ src/main/java/com/enterprise/core/TripleDouble.java
@@ -6,6 +6,7 @@ public class TripleDouble {
 
 	public int find(int first, int second) {
 		// convert to strings
+		//this is after updating 
 		// loop thru first string chars to find 3 consecutive same char
 			// if found 3 consecutive same char then
 				// if the same char is repeated two times in second string then
